a560b98a85e2cc5db26bf3adce5b4394a2095e0f
Roberto <betinhosp2007@gmail.com>
2024-10-18 09:25:54 -0300

    att

---
+++ src/repository/visitaRepository.js
@@ -0,0 +1,26 @@
+import con from "./connection";
+
+export async function cadastrarVisita(visita){
+    const comando = ` INSERT INTO tb_visita(dt_visita, id_cliente)
+                                    VALUES(?, ?)`
+
+    let resp = await con.query(comando, [visita.data, visita.cliente])
+    return resp[0].insertId
+}
+
+
+export async function deletarVisita(id_visita){
+    const comando = ` DELETE * FROM tb_visita
+                            WHERE id_visita = ?`
+
+    let resp = con.query(comando,[id_visita])
+    return resp[0] 
+}
+
+export async function pegarVisita(id_visita){
+    const comando = `SELECT * FROM tb_visita
+                            WHERE id_visita = ?`
+
+    let resp = con.query(comando, [id_visita])
+    return resp[0]
+}
